84847af32cb9cd5b079755a80c7ef84d8d09aa05
ronnybrunswijk <rienk.boonstra@gmail.com>
2015-04-09 18:20:15 +0000

    Reparaasje test

--- test/controllers/questions_controller_test.rb
+++ test/controllers/questions_controller_test.rb
@@ -9,12 +9,23 @@ class QuestionsControllerTest < ActionController::TestCase
         assert assigns(:question)
     end
 
-    test "post invalid question" do
+    test "post question title and description too short" do
         post :preview, question: { title: '', description: '' }
         assert_template :new
         question =  assigns(:question)
         refute question.valid?
         assert_equal 2, question.errors.count
+        assert_equal "is te kort (minimaal 1 teken)", question.errors[:title].first
+        assert_equal "is te kort (minimaal 1 teken)", question.errors[:description].first
     end
- 
+
+    test "post question title and description too long" do
+            post :preview, question: { title: 'a' * 101 , description: 'a' * 501 }
+            assert_template :new
+            question =  assigns(:question)
+            refute question.valid?
+            assert_equal 2, question.errors.count
+            assert_equal "is te lang (maximaal 100 tekens)", question.errors[:title].first
+            assert_equal "is te lang (maximaal 500 tekens)", question.errors[:description].first
+     end
 end
