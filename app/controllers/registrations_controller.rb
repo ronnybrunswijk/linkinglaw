b35a4592885f410dcbb8ade3d53d143529276f7d
ronnybrunswijk <rienk.boonstra@gmail.com>
2015-04-29 19:52:33 +0000

    Feskil registratie via ajax as net

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,22 +4,41 @@ class RegistrationsController < Devise::RegistrationsController
 
       build_resource(sign_up_params)
 
-      resource.skip_confirmation! if request.xhr?
-
-      if resource.save
-         if resource.active_for_authentication?
-            set_flash_message :notice, :signed_up if is_navigational_format?
-            sign_up(resource_name, resource)
-            return render :json => {:success => true}
+      if request.xhr?
+         resource.skip_confirmation! 
+         if resource.save
+            if resource.active_for_authentication?
+               set_flash_message :notice, :signed_up if is_navigational_format?
+               sign_up(resource_name, resource)
+               return render :json => {:success => true}
+            else
+               set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
+               expire_session_data_after_sign_in!
+               return render :json => {:success => true}
+            end
          else
-            set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
-            expire_session_data_after_sign_in!
-            return render :json => {:success => true}
+            clean_up_passwords resource
+            return render :json => {:success => false}
          end
-       else
-          clean_up_passwords resource
-          return render :json => {:success => false}
-       end
+      else
+         resource.save
+         yield resource if block_given?
+         if resource.persisted?
+            if resource.active_for_authentication?
+               set_flash_message :notice, :signed_up if is_flashing_format?
+               sign_up(resource_name, resource)
+               respond_with resource, location: after_sign_up_path_for(resource)
+            else
+               set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
+               expire_data_after_sign_in!
+               respond_with resource, location: after_inactive_sign_up_path_for(resource)
+            end
+         else
+            clean_up_passwords resource
+            set_minimum_password_length
+            respond_with resource
+         end
+      end
    end
 
 
