0c7805d1f43ad7a15de50dbbcd48e84769a0b082
ronnybrunswijk <rienk.boonstra@gmail.com>
2015-04-29 06:53:45 +0000

    Registraasje testke

--- app/assets/javascripts/sign_in.js.coffee
+++ app/assets/javascripts/sign_in.js.coffee
@@ -1,9 +1,10 @@
 $(document).on 'page:change', ->
-  $("#sign_in_user").bind "ajax:success", (e, data, status, xhr) ->
+  $("#sign_in_user, #new_user").bind "ajax:success", (e, data, status, xhr) ->
+      console.log("success")
       update_auth_token(xhr)
       $("#create_question").click()
 
-  $("#sign_in_user").bind "ajax:error", (e, data, status, xhr) ->
+  $("#sign_in_user, #new_user").bind "ajax:error", (e, data, status, xhr) ->
     console.log('data: ' + data)
     console.log('status: ' + status)
     console.log('xhr: ' + xhr)
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,3 +1,35 @@
 class RegistrationsController < Devise::RegistrationsController
-  respond_to :json
+#  respond_to :json
+
+   def create
+      puts "stadium 1"
+      build_resource(sign_up_params)
+
+      if resource.save
+         puts "stadium 2"
+         if resource.active_for_authentication?
+            puts "stadium 3"
+            set_flash_message :notice, :signed_up if is_navigational_format?
+            sign_up(resource_name, resource)
+            return render :json => {:success => true}
+         else
+            puts "stadium 4"
+            set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
+            expire_session_data_after_sign_in!
+            return render :json => {:success => true}
+         end
+       else
+          puts "stadium 5"
+          clean_up_passwords resource
+          return render :json => {:success => false}
+       end
+   end
+
+
+  # Signs in a user on sign up. You can overwrite this method in your own
+  # RegistrationsController.
+  def sign_up(resource_name, resource)
+     puts "stadium 6"
+     sign_in(resource_name, resource)
+  end
 end
