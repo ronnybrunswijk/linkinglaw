2856607a347499e09775c7a8b06a26e37e113619
ronnybrunswijk <rienk.boonstra@gmail.com>
2015-04-29 18:37:24 +0000

    Formulieren even herordene

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,27 +1,22 @@
 class RegistrationsController < Devise::RegistrationsController
-#  respond_to :json
 
    def create
-      puts "stadium 1"
+
       build_resource(sign_up_params)
 
       resource.skip_confirmation! if request.xhr?
 
       if resource.save
-         puts "stadium 2"
          if resource.active_for_authentication?
-            puts "stadium 3"
             set_flash_message :notice, :signed_up if is_navigational_format?
             sign_up(resource_name, resource)
             return render :json => {:success => true}
          else
-            puts "stadium 4"
             set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
             expire_session_data_after_sign_in!
             return render :json => {:success => true}
          end
        else
-          puts "stadium 5"
           clean_up_passwords resource
           return render :json => {:success => false}
        end
@@ -31,7 +26,6 @@ class RegistrationsController < Devise::RegistrationsController
   # Signs in a user on sign up. You can overwrite this method in your own
   # RegistrationsController.
   def sign_up(resource_name, resource)
-     puts "stadium 6"
      sign_in(resource_name, resource)
   end
 end
