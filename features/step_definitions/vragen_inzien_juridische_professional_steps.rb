f6f7fd89d5fc0addcbb49159f0a3bc8657d55642
ronnybrunswijk <rienk.boonstra@gmail.com>
2015-06-08 14:55:40 +0000

    Neie cucumber test foar 't ynsjin fan fraachen foar de juridyske professional.

---
+++ features/step_definitions/vragen_inzien_juridische_professional_steps.rb
@@ -0,0 +1,12 @@
+Stel(/^als juridische professional ga ik naar de 'gestelde vragen' pagina$/) do
+  visit '/questions/list'
+  page.find("title", I18n.t(:all, scope: [:questions]), visible: false)
+  assert_equal "lawyer", @current_user.role
+end
+
+Dan(/^verwacht ik een overzicht van vragen die ondernemers onlangs hebben gesteld$/) do
+  questions = Question.all
+  questions.each do |question|
+    assert page.find("a", text: question.title)
+  end 
+end
