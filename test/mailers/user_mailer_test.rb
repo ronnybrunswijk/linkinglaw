9654cf72ed1ebfa01d91e87407fd1ccb02b3d4b2
ronnybrunswijk <rienk.boonstra@gmail.com>
2015-06-10 11:40:14 +0000

    Email foar de juridyske professional

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -5,9 +5,10 @@ class UserMailerTest < ActionMailer::TestCase
    def setup
        DatabaseCleaner.start
        @user = FactoryGirl.create(:entrepreneur_with_1_question)
+       @lawyer = FactoryGirl.create(:lawyer_with_profile)
    end
 
-   def test_confirm_email
+   def test_confirm_question_email_for_entrepeneur
 
         question = @user.questions.first
         email = UserMailer.confirm_question(@user, question).deliver!
@@ -16,6 +17,10 @@ class UserMailerTest < ActionMailer::TestCase
         assert_equal [@user.email], email.to
         assert_equal 'Uw vraag is op LinkingLaw geplaatst', email.subject
 
+   end
+   
+   def test_confirm_signup_mail_for_lawyer
+   
    end
 
    def teardown
