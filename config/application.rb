f0d8ea44c7a0bdd920c8fb838e1402ab9659f3b0
ronnybrunswijk <rienk.boonstra@gmail.com>
2015-10-23 13:21:22 +0000

    Oerbodige kode ferwydere

--- config/application.rb
+++ config/application.rb
@@ -1,10 +1,6 @@
 require File.expand_path('../boot', __FILE__)
 
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "sprockets/railtie"
-require "minitest/rails/railtie"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -12,10 +8,6 @@ Bundler.require(*Rails.groups)
 
 module Linkinglaw
   class Application < Rails::Application
-
-    config.generators do |g|
-      g.test_framework :mini_test
-    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
