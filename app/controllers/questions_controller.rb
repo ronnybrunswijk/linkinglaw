5e27c8300361650f8dd049278797fa704e97ebae
ronnybrunswijk <rienk.boonstra@gmail.com>
2016-07-01 14:15:32 +0000

    Temorarily deliver e-mail directly

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -24,7 +24,8 @@ class QuestionsController < ApplicationController
     @question.user = current_user
     if params[:create]
       flash[:notice] = t(:save_success, scope: [:questions, :notifications]) if @question.save
-      UserMailer.delay.confirm_question(current_user, @question)
+#      UserMailer.delay.confirm_question(current_user, @question)
+      UserMailer.confirm_question(current_user, @question).deliver!      
       SendNotificationsJob.perform_async(@question.id)
       respond_with(@question)
     else 
